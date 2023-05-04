dbf265f52492d7cf3fa3fd9a7587ec5ad7fdcda9
jaques-l <jaques.l@outlook.com>
2023-05-04 18:55:33 -0300

    Finalização desafio do dicionario

---
+++ Dinossauro/js/main.js
@@ -0,0 +1,44 @@
+jQuery(function($){
+    const $btn = $('#btn')
+    const result = $('#result')
+    const insertTemplate = $('#insert-template')
+    const resultTemplate = $('#result-template')
+
+    $btn.on('click', function(e){
+        e.preventDefault()
+        const $input = $('#word').val()
+
+        if(/^[a-zA-Z-]*$/.test($input) == false) {
+            removeHtml()
+            alert('Apenas palavras, sem numeros ou caracteres especiais Vini');
+            return false
+        }
+
+        removeHtml()
+
+        if(!localStorage.getItem($input + '-01')){
+
+            result.append(insertTemplate.html())
+
+            $('#insert-btn').on('click', function(e){
+                 e.preventDefault()
+                 localStorage.setItem($input + '-01', $('#first-value').val())
+                 localStorage.setItem($input + '-02', $('#second-value').val())
+                 alert(`Você definiu novos significados para a palavra ${$input}.`)
+                 location.reload()
+            })
+        }else{
+            result.append(resultTemplate.html())
+            let firstMeaning = localStorage.getItem($input + '-01')
+            let secondMeaning = localStorage.getItem($input + '-02')
+
+            $('#one').append(firstMeaning)
+            $('#two').append(secondMeaning)
+        }
+    })
+
+    function removeHtml(){
+        $('#result-html').remove()
+        $('#insert-html').remove()
+    }
+})
