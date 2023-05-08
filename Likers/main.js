fc8daffc3e607609139e1c5e8c144eec574d9476
jaques-l <jaques.l@outlook.com>
2023-05-08 12:49:40 -0300

    finalização desafio Likes

---
+++ Likers/main.js
@@ -0,0 +1,32 @@
+jQuery(function($){
+
+    const $continer = $('#container');
+
+    $('#form').on('submit', function(e){
+        e.preventDefault();
+        const names = $('#names-input').val();
+        
+        
+        if(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇ, ]*$/.test(names) === false || names === ',' || names === ' '){
+            alert('porra vini, nome próprio não tem número nem caractere especial')
+            return false
+        }
+        const myArray = names.split(",");
+
+        $(this).remove()
+        
+        if(names === ''){
+            $continer.append(`<h1>no one likes this</h1>`)
+        }else if(myArray.length === 1){
+            $continer.append(`<h1>${myArray[0]}  likes this</h1>`)
+        }else if(myArray.length === 2){
+            $continer.append(`<h1>${myArray[0]} and ${myArray[1]} likes this</h1>`)
+        }else if(myArray.length === 3){
+            $continer.append(`<h1>${myArray[0]}, ${myArray[1]} and ${myArray[2]} likes this</h1>`)
+        }else{
+            $continer.append(`<h1>${myArray[0]}, ${myArray[1]} and ${myArray.length - 2} others like this</h1>`)
+        }
+                
+    })
+
+})
